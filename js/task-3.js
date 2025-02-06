db30f9d604cb0544b5337eefb887174c8ff28268
Mariia Zhuk <mariia.zhuk1023@gmail.com>
2025-02-06 23:29:39 +0200

    add all tasks

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+  const numbersArray = [];
+  for (const number of numbers) {
+    if (number > value) {
+      numbersArray.push(number);
+    }
+  }
+  return numbersArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
